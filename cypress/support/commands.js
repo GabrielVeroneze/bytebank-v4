6d78ef9cfc923fe411506e7f2096d578152a0670
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-10-30 16:21:17 -0300

    :sparkles: feat: adiciona comando customizado de login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,12 @@ Cypress.Commands.add('getByData', (seletor) => {
 Cypress.Commands.add('verificaTexto', (seletor, texto) => {
     cy.get(`${seletor}`).contains(`${texto}`)
 })
+
+Cypress.Commands.add('login', (email, senha) => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type(email)
+    cy.getByData('senha-input').type(senha)
+    cy.getByData('botao-enviar').click()
+    cy.url().should('contain', '/home')
+})
