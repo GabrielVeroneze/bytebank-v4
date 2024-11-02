1f9e829711dabda5f3f431f18ca263be52f6f435
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-11-01 22:22:08 -0300

    :test_tube: test: adiciona teste para atualização de usuário com método PUT

--- cypress/e2e/api/api-users.cy.js
+++ cypress/e2e/api/api-users.cy.js
@@ -55,4 +55,24 @@ describe('Realizando requisições para a API', () => {
                 .should('contain.text', 'Bem vindo de volta!')
         })
     })
+
+    context('Teste método PUT da API Usuários', () => {
+        it('Atualiza informações do usuário com sucesso', () => {
+            const usuario = {
+                nome: 'Marcos Vinicius Neves',
+                senha: '123456',
+            }
+
+            cy.request({
+                method: 'PUT',
+                url: 'http://localhost:8000/users/c691fd15-dcd5-4f24-89da-cdfa3cef9d67',
+                body: usuario,
+                failOnStatusCode: false,
+            }).then(response => {
+                expect(response.status).to.eq(200)
+                expect(response.body.nome).to.eq(usuario.nome)
+                expect(response.body.senha).to.eq(usuario.senha)
+            })
+        })
+    })
 })
